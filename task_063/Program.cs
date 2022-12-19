b9af6f70310b579aa24752061ee62c775c196d10
Haman692 <haman692@yandex.ru>
2022-12-20 00:40:06 +0700

    Task_063,065

---
+++ task_063/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+
+int AddNumber(string meseg)
+{
+    System.Console.WriteLine(meseg);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+void NumberPlus(int num, int count)
+{
+    if (count <= num)
+    {
+        System.Console.Write(count + " ");
+        count++;
+        NumberPlus(num, count);
+    }
+
+}
+int count = 1;
+int number = AddNumber("Введите число");
+NumberPlus(number, count);
---
+++ task_065/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+int AddNumber(string meseg)
+{
+    System.Console.WriteLine(meseg);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+void NumberPlus(int num, int count)
+{
+    if (count <= num)
+    {
+        System.Console.Write(count + " ");
+        count++;
+        NumberPlus(num, count);
+    }
+
+}
+int number = AddNumber("Введите число");
+int count = AddNumber("Введите второе число");
+NumberPlus(number, count);
